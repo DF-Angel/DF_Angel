6b275261ccb0e21ad7058904fcda94c1693841e1
ukkiyeon <ukkiyeon@naver.com>
2023-11-27 21:58:00 +0900

    UI 수정 - Root_Scan, 정렬

--- UI.py
+++ UI.py
@@ -7,25 +7,33 @@ import textwrap
 from Root_Scan import Root_Scan
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QMenuBar, QAction, QTreeView, QTreeWidget, QTreeWidgetItem,
                              QTableWidget, QTableWidgetItem, QLabel, QTextEdit, QVBoxLayout, QHBoxLayout,
-                             QWidget, QFileDialog, QMessageBox, QGridLayout, QHeaderView, QTextBrowser)
-from PyQt5.QtCore import Qt
+                             QWidget, QFileDialog, QMessageBox, QGridLayout, QHeaderView, QTextBrowser, QTableView)
+from PyQt5.QtCore import Qt, QSortFilterProxyModel
 from PyQt5.QtCore import pyqtSignal
 import sqlite3
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
 class UI_main(QMainWindow):
-    def __init__(self): # 생성자
-        super().__init__() # QMainWindow 초기화 호출
+    def __init__(self):  # 생성자
+        super().__init__()  # QMainWindow 초기화 호출
+
+        # 정렬 기능 위한
+        self.model = QStandardItemModel()
+        self.proxy_model = QSortFilterProxyModel()
+        self.proxy_model.setSourceModel(self.model)
+        self.blocktable = QTableView()  # QTableView로 수정
+        self.blocktable.setModel(self.proxy_model)
 
         self.files = {}  # 파일 경로와 트리 항목(ID)을 저장하는 딕셔너리
-        self.filename = "" # 파일 이름을 담을 전역 변수
-        self.init_ui() # ui 실행 함수 호출
+        self.filename = ""  # 파일 이름을 담을 전역 변수
+        self.init_ui()  # ui 실행 함수 호출
 
         # 생성자에서 itemSelectionChanged 시그널을 연결
         self.tree.itemSelectionChanged.connect(self.on_tree_select)
 
     def init_ui(self):
         self.setWindowTitle("IDIS DVR Analyzer by DF_Angel")
-        self.setGeometry(100, 100, 1000, 600)  # x, y, 가로, 세로
+        self.setGeometry(100, 100, 1200, 600)  # x, y, 가로, 세로
 
         # Main widget and layout
         mainWidget = QWidget(self)  # 창 생성
@@ -54,13 +62,17 @@ class UI_main(QMainWindow):
         leftLayout.addLayout(treeLayout, 1)
 
         # 오른쪽 영역 - mainLayout
-        self.blocktable = QTableWidget()
-        self.blocktable.setColumnCount(8)  # 8개 컬럼
-        self.blocktable.setHorizontalHeaderLabels(
+        # self.blocktable = QTableWidget() # 정렬 기능하려고 일단 기존 QTableWidget 주석 처리
+        self.model.setColumnCount(8)  # 모델에 컬럼 추가
+        self.model.setHorizontalHeaderLabels(
             ["Index", "Block", "Channel", "Start Time", "End Time", "Start Offset", "End Offset", "Size"])
         self.blocktable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.blocktable.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
-        mainLayout.addWidget(self.blocktable, 1) # 일단 1로
+        self.blocktable.verticalHeader().setVisible(False)  # Hide the vertical header
+        mainLayout.addWidget(self.blocktable, 1)  # 일단 1로
+
+        # 각 컬럼 헤더를 클릭했을 때 정렬 기능 추가
+        self.blocktable.setSortingEnabled(True)
 
         ''' 일단 프리뷰 없앰
         self.preview = QLabel("Preview")
@@ -79,11 +91,11 @@ class UI_main(QMainWindow):
         self.hex_offset.setStyleSheet("color: blue")
         self.hex_offset.setReadOnly(True)  # 읽기 전용으로 설정 (편집 불가능)
         self.hex_offset.setLineWrapMode(QTextEdit.NoWrap)  # 자동 줄 바꿈 비활성화
-        self.hex_offset.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff) #
-        hexoffsetLayout.addWidget(self.hex_offset, 8) #
+        self.hex_offset.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)  #
+        hexoffsetLayout.addWidget(self.hex_offset, 8)  #
         hexLayout.addLayout(hexoffsetLayout, 2)
 
-        #====================================
+        # ====================================
 
         self.hex_display_header = QLabel("00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F")  # 새로운 QLabel 위젯 생성
         self.hex_display_header.setStyleSheet("background-color: white")
@@ -91,7 +103,7 @@ class UI_main(QMainWindow):
 
         self.hex_display = QTextBrowser()
         self.hex_display.setStyleSheet("background-color: #CCCCCC")
-        self.hex_display.setReadOnly(True) # 읽기 전용으로 설정 (편집 불가능)
+        self.hex_display.setReadOnly(True)  # 읽기 전용으로 설정 (편집 불가능)
         self.hex_display.setLineWrapMode(QTextBrowser.NoWrap)  # 자동 줄 바꿈 비활성화
         self.hex_display.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)  #
         hexdisplayLayout.addWidget(self.hex_display, 8)  #
@@ -119,13 +131,13 @@ class UI_main(QMainWindow):
         self._create_menubar()
 
     def _create_menubar(self):
-        menubar = self.menuBar() # 현재 윈도우에 대한 메뉴바 가져옴 / self: 현재 클래스의 인스턴스
+        menubar = self.menuBar()  # 현재 윈도우에 대한 메뉴바 가져옴 / self: 현재 클래스의 인스턴스
 
         # File
         fileMenu = menubar.addMenu('File')
         loadAction = QAction('Load Image', self)
-        loadAction.triggered.connect(self.open_image) # Load Image 액션의 트리거 시그널이 발생 시 self.open_image 메서드 호출
-        fileMenu.addAction(loadAction) # "File" 메뉴에 방금 생성한 "Load Image" 액션을 추가
+        loadAction.triggered.connect(self.open_image)  # Load Image 액션의 트리거 시그널이 발생 시 self.open_image 메서드 호출
+        fileMenu.addAction(loadAction)  # "File" 메뉴에 방금 생성한 "Load Image" 액션을 추가
 
         # Search
         searchMenu = menubar.addMenu('Search')
@@ -140,45 +152,49 @@ class UI_main(QMainWindow):
         aboutMenu = menubar.addMenu('About')
 
     def open_image(self):
-        filepath, _ = QFileDialog.getOpenFileName(self, "Open file", "", "All Files (*)")  # filepath에 경로 저장
+        try:
+            filepath, _ = QFileDialog.getOpenFileName(self, "Open file", "", "All Files (*)")  # filepath에 경로 저장
 
-        self.filename = filepath.split("/")[-1]  # 경로에서 파일 이름만 추출해 전역변수 저장
+            self.filename = filepath.split("/")[-1]  # 경로에서 파일 이름만 추출해 전역변수 저장
 
-        df = Root_Scan(filepath)  # Root_Scan()에 경로 넘기고 객체로 받기
+            df = Root_Scan(filepath)  # Root_Scan()에 경로 넘기고 객체로 받기
 
-        if df.check_G2FDb() == 0:  # 파일이 정상적으로 열렸는지 확인
-            print("Invalid G2FDb image file. Exiting.")
-            sys.exit()  # 프로그램 종료
+            if df.check_G2FDb() == 0:  # 파일이 정상적으로 열렸는지 확인
+                print("Invalid G2FDb image file. Exiting.")
+                sys.exit()  # 프로그램 종료
 
-        df.analyzer()  # Root_Scan 클래스의 analyzer 메소드 호출해 파일 분석, db 생성
+            df.analyzer()  # Root_Scan 클래스의 analyzer 메소드 호출해 파일 분석, db 생성
 
-        if filepath:
-            item = QTreeWidgetItem(self.tree) # 새로운 트리 항목(item) 생성
-            item.setText(0, self.filename)  # 파일 이름을 트리에 추가
-            self.files[filepath] = item  # self.files 딕셔너리에 경로(키)와 해당 트리 뷰 항목(값) 저장
-            # print(self.files.items())
+            if filepath:
+                item = QTreeWidgetItem(self.tree)  # 새로운 트리 항목(item) 생성
+                item.setText(0, self.filename)  # 파일 이름을 트리에 추가
+                self.files[filepath] = item  # self.files 딕셔너리에 경로(키)와 해당 트리 뷰 항목(값) 저장
+                # print(self.files.items())
 
-            # Hex View
-            with open(filepath, 'rb') as file:
-                hex_value = file.read(5000).hex()
-                formatted_hex_lines = self.format_hex_lines(hex_value)  # 포맷된 라인 리스트로 받음
+                # Hex View
+                with open(filepath, 'rb') as file:
+                    hex_value = file.read(5000).hex()
+                    formatted_hex_lines = self.format_hex_lines(hex_value)  # 포맷된 라인 리스트로 받음
 
-                self.display_hex_value(formatted_hex_lines)
-                self.update_hex_offset(formatted_hex_lines)
-                self.display_ascii(formatted_hex_lines)
+                    self.display_hex_value(formatted_hex_lines)
+                    self.update_hex_offset(formatted_hex_lines)
+                    self.display_ascii(formatted_hex_lines)
 
-        db_filepath = './IDIS_FS_sqlite.db'
-        self.process_file(db_filepath)  # 파일 처리 메서드 호출
-        self.show_warning_message(filepath)
+            db_filepath = './IDIS_FS_sqlite.db'
+            self.process_file(db_filepath)  # 파일 처리 메서드 호출
+            self.show_warning_message(filepath)
 
-        self.Root_Scan(filepath)
+            self.Root_Scan(filepath)
+
+        except Exception as e:
+            print(f"An error occurred in open_image: {e}")
 
     # 트리에서 파일명 클릭 시
     def Root_Scan(self, filepath):
         print("again Root_Scan")
 
-        self.blocktable.setColumnCount(8)  # 8개 컬럼
-        self.blocktable.setHorizontalHeaderLabels(
+        self.model.setColumnCount(8)  # 모델에 있는 컬럼 수 설정
+        self.model.setHorizontalHeaderLabels(
             ["Index", "Block", "Channel", "Start Time", "End Time", "Start Offset", "End Offset", "Size"])
         self.blocktable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.blocktable.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
@@ -186,16 +202,16 @@ class UI_main(QMainWindow):
         db_filepath = './IDIS_FS_sqlite.db'
         self.process_file(db_filepath)  # 파일 처리 메서드 호출
 
-    def process_file(self, db_filepath): # db에서 가져와서 보여주는
-        self.blocktable.setRowCount(0)
-
+    def process_file(self, db_filepath):
         # SQLite DB와 연결 생성
         connection = sqlite3.connect(db_filepath)
         cursor = connection.cursor()
 
         # ROOTSCAN 테이블에 쿼리 실행 및 결과 받기
-        query = "SELECT * FROM ROOTSCAN"
+        query = "SELECT * FROM ROOT_SCAN"
         cursor.execute(query)
+
+        self.model.removeRows(0, self.model.rowCount())  # Clear existing data
 
         for row in cursor.fetchall():
             index = row[0]
@@ -207,13 +223,12 @@ class UI_main(QMainWindow):
             end_offset = row[6]
             size = row[7]
 
-            rowPosition = self.blocktable.rowCount() # 현재 행의 개수
-            self.blocktable.insertRow(rowPosition) # 새로운 행 추가
+            self.model.insertRow(self.model.rowCount())  # Insert a new row
 
             for col, value in enumerate([index, name, channel, start_time, end_time, start_offset, end_offset, size]):
-                item = QTableWidgetItem(str(value)) # str로 변환해서 넣음 / item = QTableWidgetItem(str(value) if col != 2 else value) 이걸로 하면 int 구분
-                item.setFlags(item.flags() ^ Qt.ItemIsEditable)  # Qt.ItemIsEditable 플래그 비활성화
-                self.blocktable.setItem(rowPosition, col, item) # 행, 열, QTableWidgetItem 삽입
+                item = QStandardItem(str(value))
+                item.setFlags(item.flags() ^ Qt.ItemIsEditable)
+                self.model.setItem(self.model.rowCount() - 1, col, item)  # Set data in the last row
 
         # Close the database connection
         connection.close()
@@ -223,7 +238,7 @@ class UI_main(QMainWindow):
         pass
 
     def on_tree_select(self):
-        selected_items = self.tree.selectedItems() # 현재 선택된 항목의 목록 가져오기
+        selected_items = self.tree.selectedItems()  # 현재 선택된 항목의 목록 가져오기
 
         ''' 위랑 중복이라 일단 주석 / 트리 항목 선택하면 뭐 보여줄 지? 우클릭해서 정밀 검색하도록? 
         if selected_items:
@@ -264,8 +279,8 @@ class UI_main(QMainWindow):
         # self.ascii_display.clear()
 
         # 오른쪽 영역 - mainLayout
-        self.blocktable.setColumnCount(14)  # 14개 컬럼
-        self.blocktable.setHorizontalHeaderLabels(
+        self.model.setColumnCount(14)  # 모델에 있는 컬럼 수 설정
+        self.model.setHorizontalHeaderLabels(
             ["Index", "Name", "Block", "Channel", "Start Time", "End Time", "Duration", "Start Offset", "End Offset",
              "Size", "Del Type", "I-Frame", "P-Frame", "삭제 여부"])
         self.blocktable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
@@ -288,7 +303,7 @@ class UI_main(QMainWindow):
         hex_offset_text = ""
 
         for line in formatted_hex_lines:
-            hex_offset_text += f'0x{offset:08X}\n' # offset 값을 16진수로 표현
+            hex_offset_text += f'0x{offset:08X}\n'  # offset 값을 16진수로 표현
             offset += 16
 
         self.hex_offset.setText(hex_offset_text)
@@ -383,8 +398,9 @@ class UI_main(QMainWindow):
         else:
             print("Unknown button clicked.")
 
+
 if __name__ == '__main__':
-    app = QApplication(sys.argv) # PyQt 애플리케이션 시작위해 PyQt의 QApplication 클래스를 인스턴스화
-    ex = UI_main() # UI_main 클래스의 인스턴스를 생성합니다. 이것은 PyQt에서 사용자 인터페이스를 정의하는 클래스일 것으로 예상됩니다.
-    ex.show() # 생성된 UI 화면 표시
+    app = QApplication(sys.argv)  # PyQt 애플리케이션 시작위해 PyQt의 QApplication 클래스를 인스턴스화
+    ex = UI_main()  # UI_main 클래스의 인스턴스를 생성합니다. 이것은 PyQt에서 사용자 인터페이스를 정의하는 클래스일 것으로 예상됩니다.
+    ex.show()  # 생성된 UI 화면 표시
     sys.exit(app.exec_())
