7f3d8d6de62830565ddce6db74da2faeecdb5d61
ukkiyeon <ukkiyeon@naver.com>
2023-11-27 18:54:01 +0900

    UI 수정 - Tree, Precise Scan

--- UI.py
+++ UI.py
@@ -14,9 +14,10 @@ import sqlite3
 
 class UI_main(QMainWindow):
     def __init__(self): # 생성자
-        super().__init__() # QMainWindow 클래스의 초기화를 수행함. 즉 QMainWindow의 초기화를 호출
+        super().__init__() # QMainWindow 초기화 호출
 
-        self.files = {}  # 파일 경로와 트리 항목(ID)을 저장하는 딕셔너리. 현재 클래스의 인스턴스 변수로, 빈 딕셔너리로 초기화
+        self.files = {}  # 파일 경로와 트리 항목(ID)을 저장하는 딕셔너리
+        self.filename = "" # 파일 이름을 담을 전역 변수
         self.init_ui() # ui 실행 함수 호출
 
         # 생성자에서 itemSelectionChanged 시그널을 연결
@@ -48,21 +49,22 @@ class UI_main(QMainWindow):
 
         # 왼쪽 영역
         self.tree = QTreeWidget()
-        self.tree.setHeaderLabel('IDIS DVR Image List')
+        self.tree.setHeaderLabel('Add IDIS DVR Image')
         treeLayout.addWidget(self.tree, 1)
         leftLayout.addLayout(treeLayout, 1)
 
         # 오른쪽 영역 - mainLayout
         self.blocktable = QTableWidget()
-        self.blocktable.setColumnCount(9)  # 9개 컬럼
+        self.blocktable.setColumnCount(8)  # 8개 컬럼
         self.blocktable.setHorizontalHeaderLabels(
-            ["Index", "Name", "Channel", "Start Time", "End Time", "Total Time", "Start Offset", "End Offset", "Size"])
+            ["Index", "Block", "Channel", "Start Time", "End Time", "Start Offset", "End Offset", "Size"])
         self.blocktable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.blocktable.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
-        mainLayout.addWidget(self.blocktable, 4)
+        mainLayout.addWidget(self.blocktable, 1) # 일단 1로
 
+        ''' 일단 프리뷰 없앰
         self.preview = QLabel("Preview")
-        mainLayout.addWidget(self.preview, 1)
+        mainLayout.addWidget(self.preview, 0) '''
 
         rightLayout.addLayout(mainLayout, 2)
 
@@ -116,10 +118,6 @@ class UI_main(QMainWindow):
         self.ascii_display.verticalScrollBar().valueChanged.connect(self.on_ascii_display_scroll)
         self._create_menubar()
 
-        # Show the warning message after process_file
-        # self.show_warning_message()
-
-
     def _create_menubar(self):
         menubar = self.menuBar() # 현재 윈도우에 대한 메뉴바 가져옴 / self: 현재 클래스의 인스턴스
 
@@ -142,39 +140,60 @@ class UI_main(QMainWindow):
         aboutMenu = menubar.addMenu('About')
 
     def open_image(self):
-        filepath, _ = QFileDialog.getOpenFileName(self, "Open file", "", "All Files (*)")  # 이미지 파일 선택, filepath에 경로 저장
+        filepath, _ = QFileDialog.getOpenFileName(self, "Open file", "", "All Files (*)")  # filepath에 경로 저장
 
-        try:
-            df = Root_Scan(filepath)  # Root_Scan()에 경로 넘기고 객체로 받기
+        self.filename = filepath.split("/")[-1]  # 경로에서 파일 이름만 추출해 전역변수 저장
 
-            if df.check_G2FDb() == 0:  # 파일이 정상적으로 열렸는지 확인
-                print("Invalid G2FDb image file. Exiting.")
-                sys.exit()  # 프로그램 종료
+        df = Root_Scan(filepath)  # Root_Scan()에 경로 넘기고 객체로 받기
 
-            df.analyzer()  # Root_Scan 클래스의 analyzer 메소드 호출해 파일 분석, db 생성
+        if df.check_G2FDb() == 0:  # 파일이 정상적으로 열렸는지 확인
+            print("Invalid G2FDb image file. Exiting.")
+            sys.exit()  # 프로그램 종료
 
-            if filepath:
-                filename = filepath.split("/")[-1]  # 경로에서 파일 이름만 추출
-                item = QTreeWidgetItem(self.tree)
-                item.setText(0, filename)  # 파일명을 트리에 추가
-                self.files[filepath] = item  # self.files 딕셔너리에 선택된 파일의 경로와 해당 트리 뷰 항목을 연결하여 저장
+        df.analyzer()  # Root_Scan 클래스의 analyzer 메소드 호출해 파일 분석, db 생성
 
-            db_filepath = './IDIS_FS_sqlite.db'
-            self.process_file(db_filepath)  # 파일 처리 메서드 호출
-            self.show_warning_message()  # Show the warning message after process_file
-            # self.update_preview(db_filepath) # 미리보기 메서드 호출
+        if filepath:
+            item = QTreeWidgetItem(self.tree) # 새로운 트리 항목(item) 생성
+            item.setText(0, self.filename)  # 파일 이름을 트리에 추가
+            self.files[filepath] = item  # self.files 딕셔너리에 경로(키)와 해당 트리 뷰 항목(값) 저장
+            # print(self.files.items())
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
+            # Hex View
+            with open(filepath, 'rb') as file:
+                hex_value = file.read(5000).hex()
+                formatted_hex_lines = self.format_hex_lines(hex_value)  # 포맷된 라인 리스트로 받음
+
+                self.display_hex_value(formatted_hex_lines)
+                self.update_hex_offset(formatted_hex_lines)
+                self.display_ascii(formatted_hex_lines)
+
+        db_filepath = './IDIS_FS_sqlite.db'
+        self.process_file(db_filepath)  # 파일 처리 메서드 호출
+        self.show_warning_message(filepath)
+
+        self.Root_Scan(filepath)
+
+    # 트리에서 파일명 클릭 시
+    def Root_Scan(self, filepath):
+        print("again Root_Scan")
+
+        self.blocktable.setColumnCount(8)  # 8개 컬럼
+        self.blocktable.setHorizontalHeaderLabels(
+            ["Index", "Block", "Channel", "Start Time", "End Time", "Start Offset", "End Offset", "Size"])
+        self.blocktable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.blocktable.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+
+        db_filepath = './IDIS_FS_sqlite.db'
+        self.process_file(db_filepath)  # 파일 처리 메서드 호출
 
     def process_file(self, db_filepath): # db에서 가져와서 보여주는
         self.blocktable.setRowCount(0)
 
-        # Connect to the SQLite database
+        # SQLite DB와 연결 생성
         connection = sqlite3.connect(db_filepath)
         cursor = connection.cursor()
 
-        # Query to retrieve data from the allocation table
+        # ROOTSCAN 테이블에 쿼리 실행 및 결과 받기
         query = "SELECT * FROM ROOTSCAN"
         cursor.execute(query)
 
@@ -188,16 +207,13 @@ class UI_main(QMainWindow):
             end_offset = row[6]
             size = row[7]
 
-            rowPosition = self.blocktable.rowCount()
-            self.blocktable.insertRow(rowPosition)
-            self.blocktable.setItem(rowPosition, 0, QTableWidgetItem(str(index)))
-            self.blocktable.setItem(rowPosition, 1, QTableWidgetItem(name))
-            self.blocktable.setItem(rowPosition, 2, QTableWidgetItem(str(channel)))
-            self.blocktable.setItem(rowPosition, 3, QTableWidgetItem(start_time))
-            self.blocktable.setItem(rowPosition, 4, QTableWidgetItem(end_time))
-            self.blocktable.setItem(rowPosition, 5, QTableWidgetItem(str(start_offset)))
-            self.blocktable.setItem(rowPosition, 6, QTableWidgetItem(str(end_offset)))
-            self.blocktable.setItem(rowPosition, 7, QTableWidgetItem(str(size)))
+            rowPosition = self.blocktable.rowCount() # 현재 행의 개수
+            self.blocktable.insertRow(rowPosition) # 새로운 행 추가
+
+            for col, value in enumerate([index, name, channel, start_time, end_time, start_offset, end_offset, size]):
+                item = QTableWidgetItem(str(value)) # str로 변환해서 넣음 / item = QTableWidgetItem(str(value) if col != 2 else value) 이걸로 하면 int 구분
+                item.setFlags(item.flags() ^ Qt.ItemIsEditable)  # Qt.ItemIsEditable 플래그 비활성화
+                self.blocktable.setItem(rowPosition, col, item) # 행, 열, QTableWidgetItem 삽입
 
         # Close the database connection
         connection.close()
@@ -209,6 +225,7 @@ class UI_main(QMainWindow):
     def on_tree_select(self):
         selected_items = self.tree.selectedItems() # 현재 선택된 항목의 목록 가져오기
 
+        ''' 위랑 중복이라 일단 주석 / 트리 항목 선택하면 뭐 보여줄 지? 우클릭해서 정밀 검색하도록? 
         if selected_items:
             selected_item = selected_items[0]
             for filepath, item in self.files.items():
@@ -220,7 +237,42 @@ class UI_main(QMainWindow):
                         self.display_hex_value(formatted_hex_lines)
                         self.update_hex_offset(formatted_hex_lines)
                         self.display_ascii(formatted_hex_lines) #
-                    break
+                    break '''
+
+        if selected_items:
+            selected_item = selected_items[0]
+            selected_name = selected_item.text(0)
+
+            if selected_name == "Precise Scan":
+                # "Precise Scan" 항목을 선택한 경우 오른쪽 측면을 업데이트하는 메서드 호출
+                self.update_right_side_for_precise_scan()
+                print(self.files)
+
+            elif selected_name == self.filename:
+                self.Root_Scan(list(self.files.keys())[0])
+                print("filename selected: " + list(self.files.keys())[0])
+
+
+
+        else:
+            # 아무 항목도 선택하지 않은 경우 처리
+            pass
+
+    def update_right_side_for_precise_scan(self):
+        # self.hex_display.clear() # 기존 내용 지우기
+        # self.hex_offset.clear()
+        # self.ascii_display.clear()
+
+        # 오른쪽 영역 - mainLayout
+        self.blocktable.setColumnCount(14)  # 14개 컬럼
+        self.blocktable.setHorizontalHeaderLabels(
+            ["Index", "Name", "Block", "Channel", "Start Time", "End Time", "Duration", "Start Offset", "End Offset",
+             "Size", "Del Type", "I-Frame", "P-Frame", "삭제 여부"])
+        self.blocktable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.blocktable.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+
+        # 예제: "Precise Scan"을 위한 hex_display에 텍스트 설정
+        # self.hex_display.setPlainText("이것은 Precise Scan 내용입니다.")
 
     def format_hex_lines(self, hex_value):
         hex_lines = textwrap.wrap(hex_value, 32)  # textwrap.wrap을 사용하여 hex 문자열을 32자씩 분할하여 리스트 생성
@@ -263,27 +315,65 @@ class UI_main(QMainWindow):
         # Set the value of the hex_offset scrollbar to match ascii_display
         self.hex_offset.verticalScrollBar().setValue(ascii_display_scroll_value)
 
-    def show_warning_message(self):
+    def show_warning_message(self, filepath):
         # Add a warning message
         warning_message = QMessageBox()
         warning_message.setIcon(QMessageBox.Warning)
-        warning_message.setText("Warning: Performing a precise scan may take a long time.\nDo you want to proceed?")
-        warning_message.setWindowTitle("Warning")
+        warning_message.setText("정밀 스캔을 진행하시겠습니까? (시간이 오래 소요될 수 있습니다.)")
+        warning_message.setWindowTitle("정밀 스캔")
         warning_message.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-        warning_message.setDefaultButton(QMessageBox.No)
+        warning_message.setDefaultButton(QMessageBox.Yes)
 
-        # Connect the warning message buttons to functions
+        # 버튼 클릭과 on_warning_button_clicked() 연결
         warning_message.buttonClicked.connect(self.on_warning_button_clicked)
 
-        # Show the warning message
+        # 실행
         result = warning_message.exec_()
+
+        # 실행 후 일단 트리에 추가 이후 활성화 or 비활성화
+        # 트리에 파일 이름이 존재하는지 확인
+        filename = filepath.split("/")[-1]
+        items_with_filename = self.tree.findItems(filename, Qt.MatchExactly, 0)
+        print(items_with_filename)
+
+        selected_item = items_with_filename[0]
+
+        # Create Allocated and Unallocated items
+        allocated_item = QTreeWidgetItem()
+        unallocated_item = QTreeWidgetItem()
+        precise_item = QTreeWidgetItem()
+        log_item = QTreeWidgetItem()
+
+        # Set text for Allocated and Unallocated items
+        precise_item.setText(0, "Precise Scan")
+        allocated_item.setText(0, "Allocated")
+        unallocated_item.setText(0, "Unallocated")
+        log_item.setText(0, "Log")
+
+        # Add Allocated and Unallocated items to the tree
+        selected_item.addChild(precise_item)
+        selected_item.addChild(allocated_item)
+        selected_item.addChild(unallocated_item)
+        selected_item.addChild(log_item)
+
+        # Expand the selected item to show the new children
+        selected_item.setExpanded(True)
+
+        QApplication.processEvents()  # Force UI to update instantly
 
         if result == QMessageBox.Yes:
             print("User clicked Yes. Proceeding with precise scan.")
-            # Perform additional actions if needed after clicking Yes
+
+        elif result == QMessageBox.No:
+            print("트리에 비활성화 시켜야")
+
+            # 비활성화 처리
+            for i in range(selected_item.childCount()):
+                child_item = selected_item.child(i)
+                child_item.setFlags(child_item.flags() & ~Qt.ItemIsEnabled)
+
         else:
             print("User clicked No. Cancelling precise scan.")
-            # Perform additional actions if needed after clicking No
 
     def on_warning_button_clicked(self, button):
         if button.text() == "&Yes":
