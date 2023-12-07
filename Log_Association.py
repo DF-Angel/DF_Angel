303afb1e5136a77fa549b4c2790f59203ea35cb7
furd3n <furd3n@gmail.com>
2023-12-07 16:18:15 +0900

    log_association 수정

--- Log_Association.py
+++ Log_Association.py
@@ -17,7 +17,13 @@ try:
     for row in rows_precise_scan:
         combined_results.append(row[1:] + (1,))
 
-    query_log = "SELECT EVENT, NULL, NULL, NULL, DATETIME, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0 FROM LOG WHERE EVENT LIKE '%삭제%' OR EVENT LIKE '%포맷%' OR EVENT LIKE '%클립%' OR EVENT LIKE '%시스템%' OR EVENT LIKE '%설정 변경%'"
+    query_log = """
+    SELECT EVENT, NULL, NULL, NULL, DATETIME, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0
+    FROM LOG
+    WHERE (EVENT LIKE '%삭제%' OR EVENT LIKE '%포맷%' OR EVENT LIKE '%클립%' OR EVENT LIKE '%시스템%' OR EVENT LIKE '%설정 변경%')
+      AND EVENT NOT LIKE '%부분 삭제 시작%'
+      AND EVENT NOT LIKE '%부분 삭제 종료%'
+    """
     cursor.execute(query_log)
 
     rows_log = cursor.fetchall()
@@ -29,8 +35,6 @@ try:
     for row in combined_results:
         print(row)
         insert_data_association(row)
-
-
 
 except sqlite3.Error as e:
     print("SQLite 오류:", e)
