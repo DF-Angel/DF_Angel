4eb4e722f5ff2f25dcc266e3cedc370685119781
salted-mackerel <dbwns6684@gmail.com>
2023-12-12 00:12:19 +0900

    Revert "commit for pull success"
    This reverts commit 09307f08b7aadc29b525efa4a474ddb2958b249c.

--- Extract_Video.py
+++ Extract_Video.py
@@ -5,14 +5,13 @@ import datetime
 import ffmpeg
 
 # 로깅 설정
-logging.basicConfig(filename='debug.log', level=logging.DEBUG,
+logging.basicConfig(filename='debug.log', level=logging.DEBUG, 
                     format='%(asctime)s:%(levelname)s:%(message)s')
 
-
 def hex_to_datetime_le(hex_value):
-    le_hex = ''.join(reversed([hex_value[i:i + 2] for i in range(0, len(hex_value), 2)]))
+    le_hex = ''.join(reversed([hex_value[i:i+2] for i in range(0, len(hex_value), 2)]))
     int_value = int(le_hex, 16)
-
+    
     second = int_value & 0x3F
     int_value >>= 6
     minute = int_value & 0x3F
@@ -34,20 +33,19 @@ def hex_to_datetime_le(hex_value):
         # 유효하지 않은 월(month) 값에 대한 예외 처리
         logging.error(f"Invalid month value extracted: {month}")
         return None  # 또는 적절한 기본 날짜 반환
-
-
-def convert_h264_to_mp4(input_file, output_file):
+    
+def convert_h264_to_mp4(input_file, output_file, speed_factor=5.0):
     try:
         (
             ffmpeg
             .input(input_file, format='h264')
-            .output(output_file, vcodec='copy')
+            .filter('setpts', f'{speed_factor}*PTS')
+            .output(output_file, vcodec='libx264', preset='ultrafast', threads='auto')
             .run(overwrite_output=True)
         )
         logging.info(f"변환 완료: {output_file}")
     except ffmpeg.Error as e:
         logging.error(f"변환 중 오류 발생: {e}")
-
 
 def little_endian_to_int(bytes_data):
     try:
@@ -56,14 +54,13 @@ def little_endian_to_int(bytes_data):
         logging.error(f"Error converting little endian to int: {e}")
         raise
 
-
 def extract_frames(db_filepath, image_filepath, selected_rows, output_folder):
     # 'Extracted' 폴더를 생성합니다.
-    '''
+    
     extracted_folder = os.path.join(output_folder, 'Extracted')
     if not os.path.isdir(extracted_folder):
         logging.info(f"Creating extracted folder at {extracted_folder}")
-        os.makedirs(extracted_folder, exist_ok=True)'''
+        os.makedirs(extracted_folder, exist_ok=True)
 
     connection = sqlite3.connect(db_filepath)
     logging.info(f"Database connected: {db_filepath}")
@@ -81,11 +78,14 @@ def extract_frames(db_filepath, image_filepath, selected_rows, output_folder):
         start_offset = start_offset_result[0]
         metadata_offset = start_offset + 0x100000
 
+        p_frames = 0
+        i_frame_found = False
+        i_frame_count = 0
+
         frame_data_file = os.path.join(output_folder, f'block_{index}.bin')  # 'Extracted' 폴더에 프레임 데이터 파일 저장
         index_file = os.path.join(output_folder, f'index_{index}.txt')  # 'Extracted' 폴더에 인덱스 파일 저장
 
-        with open(frame_data_file, 'wb') as fd_file, open(index_file, 'w') as idx_file, open(image_filepath,
-                                                                                             'rb') as file:
+        with open(frame_data_file, 'wb') as fd_file, open(index_file, 'w') as idx_file, open(image_filepath, 'rb') as file:
             while True:
                 file.seek(metadata_offset, 0)
                 metadata = file.read(32)
@@ -93,6 +93,15 @@ def extract_frames(db_filepath, image_filepath, selected_rows, output_folder):
                 if metadata == b'\x00' * 32:
                     logging.info(f"All metadata bytes are zero at index {index}. Ending read.")
                     break
+
+                frame_type = metadata[26]
+                if frame_type == 0x00:  # I-frame
+                    if not i_frame_found:
+                        i_frame_found = True
+                    else:
+                        i_frame_count += 1
+                elif frame_type == 0x01 and i_frame_found:  # P-frame
+                    p_frames += 1
 
                 frame_start_point = little_endian_to_int(metadata[28:32])
                 frame_data_size = little_endian_to_int(metadata[16:20]) - 0x23
@@ -108,19 +117,21 @@ def extract_frames(db_filepath, image_filepath, selected_rows, output_folder):
 
                 metadata_offset += 32
 
+        frame_cycle = p_frames + 1 if i_frame_count == 0 else p_frames / i_frame_count
+        speed_factor = 25.0 / frame_cycle
+        logging.info(f"Speed factor for index {index}: {speed_factor}")
+
         # 모든 프레임 데이터가 추출된 후 MP4로 변환
         output_mp4_file = os.path.join(output_folder, 'Extracted', f'frames_{index}.mp4')
-        convert_h264_to_mp4(frame_data_file, output_mp4_file)
+        convert_h264_to_mp4(frame_data_file, output_mp4_file, speed_factor)
 
     connection.close()
     logging.info("Database connection closed.")
-
 
 def main(db_filepath, image_filepath, selected_indexes, output_folder='output'):
     logging.info("Extraction started.")
     extract_frames(db_filepath, image_filepath, selected_indexes, output_folder)
     logging.info("Extraction finished.")
-
 
 if __name__ == "__main__":
     db_filepath = './IDIS_FS_sqlite.db'
